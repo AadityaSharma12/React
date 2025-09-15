e9a29daf057b74a5f65d5afcc2c77f776a82d81a
Aaditya Sharma <aadityasharma512@gmail.com>
2025-09-15 15:24:34 +0530

    first commit

---
+++ second-react-app/create-first-comp/src/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello() {
+
+  let myName="Aaditya";
+  let fullName=()=>{
+    return "Aaditya Sharma"
+  }
+
+  return <h3>Hello this is the future speaking. I am {fullName()}</h3>
+}
+
+export default Hello;
