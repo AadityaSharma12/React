19e653e70f5eff89811894e59312de8bb96c0dbb
Aaditya Sharma <aadityasharma512@gmail.com>
2025-09-16 18:16:53 +0530

    Added NewFolder with newFile.js and README.md

--- second-react-app/create-first-comp/src/Hello.jsx
+++ second-react-app/create-first-comp/src/Hello.jsx
@@ -1,11 +1,12 @@
 function Hello() {
 
   let myName="Aaditya";
+  let num=456;
   let fullName=()=>{
     return "Aaditya Sharma"
   }
 
-  return <h3>Hello this is the future speaking. I am {fullName()}</h3>
+  return <h3>Message no:{num} I am {fullName()}</h3>
 }
 
 export default Hello;
